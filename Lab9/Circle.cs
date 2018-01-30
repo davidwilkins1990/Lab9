a05434d75a255b47056ec7bfed12b5eae5a8ab3d
David Wilkins <hire.davidwilkins@gmail.com>
2018-01-30 14:18:51 -0500

    Lab 9 finished - wilkins

---
+++ Lab9/Circle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Circle
+    {
+        private double radius = 0.0;
+        private double area = 0.0;
+        private double circumference = 0.0;
+        private const double pi = Math.PI;
+        
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double input)
+
+        {
+            this.radius = input;
+        }        
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        //circ is D * pi  - d is r*2
+
+        public double calculateCircumference()
+        {
+            double circ= 0.00;
+            circ = (this.radius * 2) * (pi);
+            this.circumference = circ;
+            return circ;
+        }
+
+        public string calculateFormattedCircumference()
+        {
+            string cfc = "";
+            cfc = ("Circumference: " + Decimal.Round((decimal)this.circumference, 2));
+            return cfc;
+        }
+
+        //area is pi * R^2
+        public double calculateArea()
+        {
+            double area= 0.0;
+            area = this.radius * this.radius * pi;
+            this.area = area;
+            return area;
+        }
+
+        public string calculateFormattedArea()
+        {
+            string cfa = "";
+            
+            cfa = ("Area: " + Decimal.Round((decimal)this.area, 2));
+            // Console.WriteLine(("{0:D2} circ"), this.circumference);
+            return cfa;
+        }
+
+        private string formatNumber(double x)
+        {
+            string fn = "";
+
+            return fn;
+        }
+
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        public static int counter = 0;
+        public static void calc( )
+        {
+            Console.Write("Enter a radius: ");
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                Circle circle = new Circle(input);
+                counter++;
+                circle.calculateArea();
+                circle.calculateCircumference();
+
+                Console.WriteLine(circle.calculateFormattedCircumference());
+                Console.WriteLine(circle.calculateFormattedArea());
+
+                Console.WriteLine("Press 'y' to continue or any other key to quit");
+                string input2 = Console.ReadLine();
+                again(input2);
+            }
+            catch(Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("\n\n****ERROR: Invalid input.*****");
+                calc();
+                
+            }
+            
+        }
+
+
+        public static void again (string input2)
+        {
+            if (input2 == "y")
+            {
+                calc();
+            }
+            else
+            {
+                Console.WriteLine("Goodbye. You made " + counter + " circle object(s)");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            
+
+            calc();
+
+            Console.ReadLine();
+        }
+    }
+}
